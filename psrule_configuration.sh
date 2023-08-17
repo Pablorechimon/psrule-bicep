31f80d9e8b0cfa8a4f16b5910467479046741496
Pablo Hernan Rechimon <PabloHernan.Rechimon_contractor+JMF@jmfamily.com>
2023-08-17 11:38:31 -0300

    Add Action

---
+++ psrule_configuration.sh
@@ -0,0 +1,5 @@
+url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/v0.0.1.tar.gz"  
+curl -L -O $url
+filename=$(basename $url)
+tar -xvzf $filename --strip-components 1 --exclude="README.md"
+ls -la
