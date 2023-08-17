786214281c30513857d2ead5a13b4846eff25a81
Pablo Hernan Rechimon <PabloHernan.Rechimon_contractor+JMF@jmfamily.com>
2023-08-17 16:13:28 -0300

    Test

--- psrule_configuration.sh
+++ psrule_configuration.sh
@@ -1,4 +1,4 @@
-url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/v1.9.tar.gz"  
+url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/v1.10.tar.gz"  
 curl -L -O $url
 filename=$(basename $url)
 tar -xvzf $filename --strip-components 1 --exclude="README.md"
