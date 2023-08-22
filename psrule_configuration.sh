ee50207132d4d54f579793fbbeb47dcc901e9e24
Pablo Hernan Rechimon <PabloHernan.Rechimon_contractor+JMF@jmfamily.com>
2023-08-22 10:06:19 -0300

    Changing to V0.1

--- psrule_configuration.sh
+++ psrule_configuration.sh
@@ -1,4 +1,4 @@
-url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/v1.1.0.tar.gz"  
+url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/v0.1.tar.gz"  
 curl -L -O $url
 filename=$(basename $url)
 tar -xvzf $filename --strip-components 1 --exclude="README.md"
