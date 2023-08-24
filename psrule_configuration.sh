c60bef16353c850dfbcc802eed34aca9f35587d2
Pablo Hernan Rechimon <46685754+Pablorechimon@users.noreply.github.com>
2023-08-24 17:17:02 -0300

    Update psrule_configuration.sh

--- psrule_configuration.sh
+++ psrule_configuration.sh
@@ -1,4 +1,4 @@
-url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/v0.1.tar.gz"  
+url="https://github.com/Pablorechimon/psrule-bicep/archive/refs/tags/latest-exclusions.tar.gz"  
 curl -L -O $url
 filename=$(basename $url)
 tar -xvzf $filename --strip-components 1 --exclude="README.md"
